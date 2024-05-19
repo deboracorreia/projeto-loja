a7969cd36e3353683840f5ce94b530d98a6af9d5
Débora Dal Correia <deeboradalcorreia@gmail.com>
2024-05-19 19:38:54 -0300

    criado paginas inicias

---
+++ source/javascript/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const contactForm = document.getElementById('contact-form');
+
+    if (contactForm) {
+        contactForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            alert('Mensagem enviada com sucesso!');
+        });
+    }
+});
